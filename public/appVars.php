d31cd859e64cd441f1fa34bd38025e74c4435813
vivekcoder123 <vivekrautela000@gmail.com>
2018-11-18 22:26:36 +0530

    tslfjd

--- public/appVars.php
+++ public/appVars.php
@@ -2,7 +2,7 @@
  define("MM_MAXFILESIZE",5076800);
  define("MM_MINIMGWIDTH",10);
  define("MM_MINIMGHEIGHT",50);
- define("SITE_BASE_URL", "http://greetstore.herokuapp.com/");
+ define("SITE_BASE_URL", "https://greetstore.herokuapp.com/");
  
 
  ?>
